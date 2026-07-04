18904749ea103b2d1431851b046b7a807bdd2312
Joel Mwala <Joelmwala7@gmail.com>
2026-07-04 23:15:42 +0200

    add meeting note form

--- src/app/pages/meeting-notes/meeting-notes.ts
+++ src/app/pages/meeting-notes/meeting-notes.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-meeting-notes',
   standalone: true,
+  imports: [FormsModule],
   templateUrl: './meeting-notes.html',
 })
-export class MeetingNotes {}
+export class MeetingNotes {
+  title = '';
+  notes = '';
+
+  addMeetingNote() {
+    console.log('Meeting title:', this.title);
+    console.log('Meeting notes:', this.notes);
+  }
+}
